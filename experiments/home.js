154fac2a667d95e52c38585c98e616c5f4982fa7
Jerrod <Jeddy13333@yahoo.com>
2021-03-12 13:32:20 -0700

    my first commit

---
+++ experiments/home.js
@@ -0,0 +1,11 @@
+console.log('hello');
+//alert('JE yall');
+
+//how to write a comment inline
+
+//Variables 
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
